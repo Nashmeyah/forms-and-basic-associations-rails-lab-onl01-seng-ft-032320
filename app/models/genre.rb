cf366c4a8d853a2b436db8a72ce6856b1e8444a8
Nashmeyah <nashalrekabi@gmail.com>
2020-06-03 16:22:22 -0600

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,11 @@
 class Genre < ActiveRecord::Base
-  # add associations
+  validates :name, presence: true
+  has_many :songs
+
+  def song_genre_ids=(ids)
+    ids.each do |id|
+      song = Song.find(id)
+      self.songs << song
+    end
+  end
 end
